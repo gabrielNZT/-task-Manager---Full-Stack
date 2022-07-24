b71b79e770786d70c370ba32fd8bd7c042241213
gabrielNZT <74830051+gabrielNZT@users.noreply.github.com>
2022-07-24 13:17:27 -0300

    commit 5 - Feature Update Group (1/2)

--- src/group/group.js
+++ src/group/group.js
@@ -3,7 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import NewCard from './newCard';
 import CloseBTN from './closeButton';
 import { Container } from './style';
-
+import Form from 'react-bootstrap/Form'
 import { useState } from 'react';
 import Task from '../card/index.js';
 
@@ -11,7 +11,7 @@ import Task from '../card/index.js';
 function Group(props) {
     
     const {group, onDeleteGroup} = props;
-   
+    const [header, setHeader] = useState(group.header);
     const [list, setList] = useState([]);
 
 
@@ -23,6 +23,9 @@ function Group(props) {
         setList(list => [...list, task]);
         //console.log(list);
     };
+    const handleOnChange = e => {
+        setHeader(e.target.value)
+    }
 
     const onEdit = item => {
         setList(list => list.map(i => {
@@ -41,9 +44,21 @@ function Group(props) {
     return (
         <Container>
             <Card border="dark" style={{ width: '18rem', marginTop: 20, marginLeft: 30, }}>
+                
 
                 <Card.Header style={{ background: '#1976d2', color: '#FFFFFF', fontSize: '26px', border: '2px solid black', display: 'flex', position: 'relative', height: 55 }}>
-                    {group.header}
+                        
+                     <Form>
+                        <Form.Label>
+                            <Form.Control style={{background: '#1976d2', border: 'transparent', fontSize: '20px', color: '#FFF'}} 
+                            onChange={handleOnChange}
+                            required
+                            type='text'
+                            value={header}
+                            />
+                        </Form.Label>
+                     </Form>
+                    
                     <CloseBTN onDeleteGroup={onDeleteGroup} group={group}></CloseBTN>
                 </Card.Header>
 
