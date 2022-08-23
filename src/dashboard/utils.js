7d8bc855185fa27da1f87a64955c290698c17c41
gabriel_nzt <gabrielnunescesarino@gmail.com>
2022-08-23 18:38:34 -0300

    commit 15 - Spring to Grails

--- src/dashboard/utils.js
+++ src/dashboard/utils.js
@@ -10,3 +10,15 @@ export function reorderGroupsCards(groups) {
     }
     );
 }
+
+export function findGroup(groups, cardId){
+    var currentGroup = null
+    groups.forEach(group => {
+        group.cards.forEach(card => {
+            if(card.id === cardId){
+              currentGroup = group
+            }
+        })
+    })
+    return currentGroup
+}
